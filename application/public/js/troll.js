3070c13d1e9013eff45844259f02f96a1463c974
fatston <cliftonkor@gmail.com>
2021-09-10 23:24:57 +0800

    edit_profile.js
    moved edit profile from troll.js to edit_profile.js

--- application/public/js/troll.js
+++ application/public/js/troll.js
@@ -66,18 +66,3 @@ async function getSellerName(target) {
     }
 }
 
-async function getSellerData() {
-    const profile_data_url = '/api/seller/';
-    const response = await fetch(profile_data_url);
-    const profileData = await response.json();
-    if (profileData.success === true) {
-        document.getElementById('username').value = profileData.username
-        document.getElementById('password').value = profileData.password
-        document.getElementById('name').value = profileData.name
-        document.getElementById('company').value = profileData.company
-        document.getElementById('email').value = profileData.email
-        document.getElementById('contact').value = profileData.contact
-    } else {
-        document.getElementById(target).textContent = "error"
-    }
-}
