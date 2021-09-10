e914693b76ea8df3e25629da322774fe25ca912e
fatston <cliftonkor@gmail.com>
2021-09-10 21:36:15 +0800

    /....

--- application/app.js
+++ application/app.js
@@ -555,8 +555,8 @@ function editSellerProfile(req,res,next) {
 
 
 
-app.listen(5000,()=>{
-    console.log('server is listening on port 5000')
+app.listen(80,()=>{
+    console.log('server is listening')
 })
 
 
