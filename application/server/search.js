589114dae237b239f8366f7f8df82df83d14a538
denny-wongso <denny.wongso@gmail.com>
2021-09-26 22:20:31 +0800

    Update listing detail ui and update query to get facilities and seller info in search.js

--- application/server/search.js
+++ application/server/search.js
@@ -2,16 +2,32 @@ const db = require('./connection')
 
 const getListingDetails = (id, res) => {
     let sql = `
-    SELECT *
-    FROM listings
-    WHERE listing_id = ?;
+    SELECT l.*, s.name, s.contact_number, f.*
+    FROM listings l, listing_facilities lf, facilities f, seller s
+    WHERE l.listing_id = lf.listing_id AND lf.facility_id = f.facility_id AND l.seller_id = s.seller_id
+    AND l.listing_id = ?
+    ORDER BY f.facility_id ASC;
     `
     db.query(sql, id, (err, row) => {
         if (err) {
             res({success:false, msg:err});
         }
         else {
-            res({success:true, data:row, msg:'listings details retrieved'});
+            //number of rows depending on the number of facilities, hence need to do a loop to seperate it
+            var listing = {};
+            const facilities = []
+            row.forEach(element => {
+                facilities.push({"facility_id": element.facility_id, 
+                                "facility_name": element.facility_name});
+                delete element.facility_id;
+                delete element.facility_name;
+                if(Object.keys(listing).length === 0) {
+                    listing = element;
+                }
+            });
+            console.log(listing)
+            listing["facilities"] = facilities;
+            res({success:true, data: [listing], msg:'listings details retrieved'});
         }
     })
 }
